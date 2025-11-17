e82432a314969eba093df1583beda683abe6db1d
Debsnil Samudra <debsnilsamudra@gmail.com>
2025-11-18 03:33:49 +0530

    fix: set Reply-To header in support emails to submitter's address

--- service/email.go
+++ service/email.go
@@ -50,23 +50,25 @@ func (es *EmailService) SendSupportEmail(name, email, message string) error {
 	subject := fmt.Sprintf("Support Request from %s", name)
 
 	// Email body with proper headers
+	// Note: From must be the authenticated email, but Reply-To is set to the submitter's email
 	body := fmt.Sprintf("From: %s\r\n"+
 		"To: %s\r\n"+
+		"Reply-To: %s\r\n"+
 		"Subject: %s\r\n"+
 		"MIME-Version: 1.0\r\n"+
 		"Content-Type: text/plain; charset=UTF-8\r\n"+
 		"\r\n"+
 		"New Support Request Received\r\n"+
 		"\r\n"+
-		"Name: %s\r\n"+
-		"Email: %s\r\n"+
+		"From: %s <%s>\r\n"+
 		"\r\n"+
 		"Message:\r\n"+
 		"%s\r\n"+
 		"\r\n"+
 		"---\r\n"+
-		"This email was sent from the support form on your website.\r\n",
-		es.fromEmail, es.toEmail, subject, name, email, message)
+		"This email was sent from the support form on your website.\r\n"+
+		"Reply to this email to respond directly to %s.\r\n",
+		es.fromEmail, es.toEmail, email, subject, name, email, message, email)
 
 	// SMTP authentication
 	auth := smtp.PlainAuth("", es.smtpUsername, es.smtpPassword, es.smtpHost)
